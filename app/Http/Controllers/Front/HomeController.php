9b0f42afeadc735f6a92bf5919d367a9674a958e
Dmitry Belousov <dmitry1998.ua@outlook.com>
2018-10-10 17:00:57 +0300

    first commit

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Shop\Categories\Repositories\Interfaces\CategoryRepositoryInterface;
+
+class HomeController extends Controller
+{
+    /**
+     * @var CategoryRepositoryInterface
+     */
+    private $categoryRepo;
+
+    /**
+     * HomeController constructor.
+     * @param CategoryRepositoryInterface $categoryRepository
+     */
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepo = $categoryRepository;
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index() {
+
+        $cat1 = $this->categoryRepo->findCategoryById(1);
+        $cat2 = $this->categoryRepo->findCategoryById(2);
+
+        return view('front.index', compact('cat1', 'cat2'));
+    }
+}
